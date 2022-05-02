7ef6ef5420a508b7fdd462204041df75cd8a54b1
Richard Clarke <freshnewrich@gmail.com>
2022-05-01 18:39:46 -0700

    floorplan more

--- src/components/Floorplan.jsx
+++ src/components/Floorplan.jsx
@@ -3,3 +3,17 @@ import LivingRoom from "./LivingRoom";
 import Bedroom from "./Bedroom";
 import Bath from "./Bath";
 import "../styles.css";
+
+export default function FloorPlan(props) {
+    return (
+      <div className="floor-plan">
+        <Bedroom bedNum="1" brNum="bedroom-1" />
+        <Kitchen />
+        <Bath size="full" />
+        <Bedroom bedNum="2" brNum="bedroom-2" />
+        <LivingRoom />
+        <Bath size="half" />
+        <Bedroom bedNum="3" brNum="bedroom-3" />
+      </div>
+    );
+  }
